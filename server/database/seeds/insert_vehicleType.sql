35d943ac8123fa5a8435cea392554c0e904042e6
Jackie <sangxu.yin@alumni.ubc.ca>
2019-11-21 11:29:35 -0800

    migration & seed: changed price dataType to 'floats' and created VehicleType seeds

---
+++ server/database/seeds/insert_vehicleType.sql
@@ -0,0 +1,7 @@
+INSERT INTO VehicleType VALUES ('economy', 'small, lightweight, inexpensive', 100, 18, 1, 10, 1.50, 0.05, 10);
+INSERT INTO VehicleType VALUES ('compact', 'built for safety and better fuel efficiency', 110, 19, 1.20, 11, 1.55, 0.07, 11.50);
+INSERT INTO VehicleType VALUES ('mid-size', 'five-inch touchscreen infotainment system', 130, 21, 1.50, 15, 1.99, 0.10, 15);
+INSERT INTO VehicleType VALUES ('standard', 'remote keyless entry', 119.99, 20, 1.30, 13, 1.29, 0.08, 12);
+INSERT INTO VehicleType VALUES ('full-size', 'remote keyless entry', 140, 23, 1.70, 16, 2, 0.15, 17);
+INSERT INTO VehicleType VALUES ('suv', 'touchscreen infotainment system', 150, 15, 1.90, 19, 2.20, 0.20, 19);
+INSERT INTO VehicleType VALUES ('truck', 'has backseats', 170, 18, 2.10, 2.40, 0.30, 22);
