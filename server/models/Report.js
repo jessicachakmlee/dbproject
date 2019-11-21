0b72f274bbfb49fa1cb84046e767ebd9d600cf09
levanayang <levanavyang@gmail.com>
2019-11-21 15:07:00 -0800

    moved report methods to another class

---
+++ server/models/Report.js
@@ -0,0 +1,56 @@
+class Report{
+    /*TODO: Make report*/
+    public static makeRentalReport(){
+
+    }
+    /*TODO: Output 1: list all rented out vehicles for this day
+            Sorting: Group entries by branch, within each branch, and vehicle category*/
+    public static displayAllRentedVehicles(timeInterval, branch) {
+
+    }
+    /*TODO: get number of vehicles for each category*/
+    public static getNumRentalsByVCategory(){
+
+    }
+
+    /*TODO: get number of rentals at each branch*/
+    public static getNumRentalsByBranch(){
+
+    }
+
+    /*TODO: get total number of rentals*/
+    public static getNumRentals(){
+
+    }
+
+    /*TODO: Output 1: list daily info on all rented out vehicles for company
+                Sorting: Group entries by branch, within each branch, and vehicle category
+    *  Output 2: number of vehicles for each vehicle category
+        Output 3: number of rentals at each branch
+        Output 4: total number of rentals*/
+    public static makeDailyRentalReport(){
+
+    }
+    /*TODO: Make daily rental report for one branch*/
+    public static makeDailyRentalBranchReport(){
+
+    }
+
+
+    /*TODO: Output 1: list daily info on all returned vehicles for company
+    *               Sorting: grouped by branch, within each branch, vehicle category
+    *       Output 2: number of vehicles returned per vehicle category
+    *       Output 3: total revenue per vehicle category
+    *       Output 4: subtotals for number of vehicles per branch and
+    *                   revenue per branch
+    *       Output 5: total number of returns*/
+    public static makeDailyReturnReport(){
+
+    }
+    /*TODO: Make daily return report for one branch*/
+    public static makeDailyReturnBranchReport(){
+
+    }
+}
+
+module.exports(Report);
