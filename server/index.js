7193507d653008db068a9e08aaf25a8662e898d0
jessicalee <jessicachakmlee@gmail.com>
2019-11-23 11:48:42 -0800

    testing heroku

--- server/index.js
+++ server/index.js
@@ -13,9 +13,19 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+// All endpoints
+app.use('/api/databaseManipulations', require('./api/databaseManipulations'));
+app.use('/api/customer', require('./api/customer'));
+app.use('/api/rent', require('./api/rent'));
+app.use('/api/reservation', require('./api/reservations'));
+app.use('/api/return', require('./api/return'));
+app.use(`/api/vehicle`, require("./api/vehicle"));
+app.use('/api/time', require("./api/timePeriod"));
+app.use(`/api/vehicleType`, require("./api/vehicleType"));
+
 if(ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/build')));
-    app.use('*', (req, res) => {
+    app.use((req, res) => {
         res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
     });
 }
@@ -38,16 +48,5 @@ db.query('SELECT current_user;', (err, res) => {
         return console.log(err.error);
     console.log(`PostgreSQL connected to user: ${res[0].current_user}`);
 });
-
-
-// All endpoints
-app.use('/api/databaseManipulations', require('./api/databaseManipulations'));
-app.use('/api/customer', require('./api/customer'));
-app.use('/api/rent', require('./api/rent'));
-app.use('/api/reservation', require('./api/reservations'));
-app.use('/api/return', require('./api/return'));
-app.use(`/api/vehicle`, require("./api/vehicle"));
-app.use('/api/time', require("./api/timePeriod"));
-app.use(`/api/vehicleType`, require("./api/vehicleType"));
 
 module.exports = app;
