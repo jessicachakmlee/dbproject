6891fe5746bf4f28fdaf1b59b37921f372aa0fdf
jessicalee <jessicachakmlee@gmail.com>
2019-11-23 01:33:32 -0800

    removed routing stuff

--- server/index.js
+++ server/index.js
@@ -13,12 +13,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-// if(ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, '../frontend/build')));
-//     app.use((req, res) => {
-//         res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
-//     });
-// }
+if(ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.use((req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+    });
+}
 
 // start listening to the port
 app.listen(PORT, () => {
