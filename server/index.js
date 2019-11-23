03c887f825caf86cbaa807c7e92e1dade1bf00c5
jessicalee <jessicachakmlee@gmail.com>
2019-11-23 01:12:43 -0800

    small changes

--- server/index.js
+++ server/index.js
@@ -14,9 +14,9 @@ app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 if(ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
     app.use((req, res) => {
-        res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
+        res.sendFile(path.join(__dirname, '/client/build/index.html'));
     });
 }
 
