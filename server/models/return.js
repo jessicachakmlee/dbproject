08afade34d2c23d2053c18434cd47fb7f9c0ec11
levanayang <levanavyang@gmail.com>
2019-11-24 17:41:38 -0800

    finished returnVehicleAsync function

--- server/models/return.js
+++ server/models/return.js
@@ -229,15 +229,13 @@ class Return {
     static insert (rid, date, time, odometer,
                    fulltank, value, callback) {
 
-        const insertQuery = {
-            text: 'INSERT INTO return(rid, date, time, odometer, ' +
+        const insertQuery = 'INSERT INTO return(rid, date, time, odometer, ' +
             'fulltank, value) ' +
-            'VALUES ($1, $2, $3, $4, $5, $6)',
-            values: [rid, date, time, odometer,
-                fulltank, value]
-        }
+            'VALUES ($1, $2, $3, $4, $5, $6) ' +
+            'RETURNING *';
+        const values = [rid, date, time, odometer, fulltank, value]
 
-        db.query(insertQuery, (err, res) => {
+        db.query(insertQuery, values, (err, res) => {
             if (err.error)
                 return callback(err);
             callback(res);
